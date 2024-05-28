3df2ab13d2b3ca18e53d68460ec7b8d060722dd3
SiaChachra <122416057+SiaChachra@users.noreply.github.com>
2024-05-28 14:44:49 -0400

    init

--- Assignment1 C++ Sia/Assignment1 C++ Sia/Assignment1.cpp
+++ Assignment1 C++ Sia/Assignment1 C++ Sia/Assignment1.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 double calculate_gibbs_free_energy(double R, double T, double lnK)
 {
-    // Modify the temperature from Celsius to Kelvin
+
     double T_kelvin = T + 273.15;
-    // Calculate Gibbs free energy
+
     double delta_G = -R * T_kelvin * lnK;
     return delta_G;
 }
@@ -15,19 +15,16 @@ double calculate_gibbs_free_energy(double R, double T, double lnK)
 int main()
 {
     double R, T;
-    const double lnK = -6.45; // Predefined value of lnK
+    const double lnK = -6.45;
 
-    // Take user inputs for R and T
     cout << "Enter the value for R: ";
     cin >> R;
 
     cout << "Enter the value for T (in Celsius): ";
     cin >> T;
 
-    // Calculate Gibbs free energy
     double delta_G = calculate_gibbs_free_energy(R, T, lnK);
 
-    // Print the result
     cout << "The Gibbs free energy is: " << delta_G << endl;
 
     return 0;
